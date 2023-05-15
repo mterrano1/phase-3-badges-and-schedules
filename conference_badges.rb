c21accaac128c95d75619efaffb84b19bed53f12
Christopher Terrano <mterrano1@yahoo.com>
2023-05-15 14:07:11 -0500

    create assign_room method

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,3 +6,9 @@ end
 def batch_badge_creator names
     names.map { |name| "Hello, my name is #{name}."} 
 end
+
+def assign_rooms speakers
+    speakers.map.with_index(1) do |speaker, room|
+        "Hello, #{speaker}! You'll be assigned to room #{room}!"
+    end
+end
